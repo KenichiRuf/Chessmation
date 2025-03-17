e66c4102504a48e8088afe457ebb8a38e0b21225
KenichiRuf <ruf.kenichi@gmail.com>
2025-03-17 11:46:56 -0400

    add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,19 @@ import { getUser } from '@/lib/db/queries';
 export const metadata: Metadata = {
   title: 'Chessmation',
   description: 'Add fun animations to your chess games.',
+  icons: {
+    icon: [
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+    ],
+    apple: [
+      { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
+    ],
+    other: [
+      { url: '/android-chrome-192x192.png', sizes: '192x192', type: 'image/png' },
+      { url: '/android-chrome-512x512.png', sizes: '512x512', type: 'image/png' },
+    ],
+  },
 };
 
 export const viewport: Viewport = {
@@ -27,6 +40,9 @@ export default function RootLayout({
       lang="en"
       className={`bg-white dark:bg-gray-950 text-black dark:text-white ${manrope.className}`}
     >
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
       <body className="min-h-[100dvh] bg-gray-50">
         <UserProvider userPromise={userPromise}>{children}</UserProvider>
       </body>
